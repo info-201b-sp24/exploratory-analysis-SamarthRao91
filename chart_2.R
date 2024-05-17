dff7f1f8b9d81fa50c8ebc3dc991fc6fd3abd6ac
nschung28 <nschung@uw.edu>
2024-05-17 11:40:32 -0700

    added chart 2/description and test index.html

---
+++ chart_2.R
@@ -0,0 +1,9 @@
+library(ggplot2)
+sleep_data <- read.csv("sleep75.csv");
+chart_2_plt <- ggplot(sleep_data, aes(x = sleep, y = lhrwage)) + 
+  geom_point(alpha = 0.7) + 
+  labs(title = "Natural Logarithm of Hourly Wage vs Sleep",
+       x = "Sleep in Minutes",
+       y = "Natural Logarithmic Wage",
+       color = "Health")
+print(chart_2_plt)
